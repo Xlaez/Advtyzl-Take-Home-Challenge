87e94c32f78d30b45e282cabb02d6242d4cc80f5
Utee <utibeabasiekong2@gmail.com>
2024-05-29 21:39:49 +0100

    update: app.module file with ssl config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,21 +8,25 @@ import { Rooms } from './rooms/rooms.entity';
 
 const env = new Environment();
 
+const postgresqlConfig: any = {
+  type: 'postgres',
+  host: env.prod() ? env.dbHostName : 'localhost',
+  port: 5432,
+  username: env.prod() ? env.dbUserName : 'user',
+  password: env.prod() ? env.dbPassword : 'password123',
+  database: env.prod() ? env.dbName : 'litpad',
+  entities: [Rooms],
+  synchronize: new Environment().dev(),
+  retryAttempts: 6,
+};
+
+if (env.prod()) {
+  postgresqlConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: env.prod() ? env.dbHostName : 'localhost',
-      port: 5432,
-      username: env.prod() ? env.dbUserName : 'user',
-      password: env.prod() ? env.dbPassword : 'password123',
-      database: env.prod() ? env.dbName : 'litpad',
-      entities: [Rooms],
-      synchronize: new Environment().dev(),
-      retryAttempts: 6,
-    }),
-    RoomsModule,
-  ],
+  imports: [TypeOrmModule.forRoot(postgresqlConfig), RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
